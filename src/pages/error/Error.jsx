4a812efa14c84fab2bc8267b34338c1c3ae77f2e
Yelhie <maelle.ew.m@gmail.com>
2023-12-19 10:30:55 +0100

    Correction CSS
    Correction CSS

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,10 +1,13 @@
+import { NavLink } from 'react-router-dom';
 import './error.scss';
 
 export default function ErrorPage() {
     return (
-        <div className="errorContainer">
-            <p className="errorNumber">404</p>
-            <p className="errorText">Oups! La page que vous demandez n'existe pas.</p>
+        <div className="error_Container">
+            <p className="error_Number">404</p>
+            <p className="error_Text">Oups! La page que vous demandez n'existe pas.</p>
+
+            <NavLink className='error_Relink' to='/'>Retourner sur la page d'accueil</NavLink>
         </div>
     );
 }
